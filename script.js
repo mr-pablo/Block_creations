896655922087cee4f0258538177f6866c89fbd16
Muhammed-Rasi <muhammedrasi121@gmail.com>
2024-01-24 17:00:49 +0530

    finish

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let colors = ["red","black","yellow"];
-let num = 0
+// let num = 0
 function add() {
     
 
     var block_div = document.createElement("div");
     block_div.className = "blocks";
     let random_color = colors[Math.floor(Math.random() * colors.length)];
+    block_div.style.background = random_color
     document.getElementById("container").appendChild(block_div);
-    var blockElements = document.getElementsByClassName("blocks");
-    blockElements[num].style.background = random_color;
-    num++
+    // var blockElements = document.getElementsByClassName("blocks");
+    // blockElements[num].style.background = random_color;
+    // num++
     
 }
 
@@ -20,17 +21,18 @@ function choose_colors(){
 
 function push_color(color){
     colors.push(color)
-    console.log(colors);
+    
    let element = document.getElementById(`${color}`)
    var displayValue = window.getComputedStyle(element).getPropertyValue('display');
+// var displayValue = element.style.display
+
    console.log(displayValue);
    if (displayValue == "block") {
     document.getElementById(`${color}`).style.display = "none"
     let remove_id = `remove_${color}`
     document.getElementById(remove_id).style.display = "block"
-   }else{
-    document.getElementById(`${color}`).style.display = "block"
    }
+
 
 }
 
